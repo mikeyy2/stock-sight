527d43c5e74d0440303d283395b1054c8b75b404
Michael Proeschel <124392897+mikeyy2@users.noreply.github.com>
2025-04-22 19:19:31 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
-from app import app
+from app.app import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render provides this automatically
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host="0.0.0.0", port=port, debug=True)
